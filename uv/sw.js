c6ee5bd699dab9929c72d4026c8162fd12b8b0b4
K.A <akkazu1121@gmail.com>
2026-02-11 12:56:09 +0900

    Update sw.js

--- uv/sw.js
+++ uv/sw.js
@@ -2,6 +2,14 @@ importScripts('./uv.bundle.js');
 importScripts('./uv.config.js');
 importScripts('./uv.sw.js');
 
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
 const uv = new UVServiceWorker();
 
 self.addEventListener('fetch', event => {
