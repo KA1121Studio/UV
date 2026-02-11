aaded188b5c8c0405a994383ca2abc3479d9279c
K.A <akkazu1121@gmail.com>
2026-02-11 12:38:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const { createBareServer } = require('@tomphttp/bare-server-node');
+
 const app = express();
+const bareServer = createBareServer('/bare/');
 
 app.use(express.static('public'));
 app.use('/uv', express.static('uv'));
+
+// Bare server を HTTP upgrade & fetch に接続
+app.use((req, res, next) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    next();
+  }
+});
+
+app.on('upgrade', (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
